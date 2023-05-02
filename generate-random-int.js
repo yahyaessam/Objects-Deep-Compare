a32eaffc63004139818804520ac4d3cc083b7538
Yahia Essam <yahia_essam@siliconexpert.com>
2023-05-02 16:17:44 +0300

    rename function name

--- generate-random-int.js
+++ generate-random-int.js
@@ -1,5 +1,5 @@
 // Function to generate random number between min and max
 
-function GenRandomIntBetween(min,max) {
+function genRandomIntBetween(min,max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
